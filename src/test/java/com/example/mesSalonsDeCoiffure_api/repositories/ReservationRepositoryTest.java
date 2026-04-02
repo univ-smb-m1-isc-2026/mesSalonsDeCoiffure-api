5d51967464927f48ffdfe4cdfc2ec510ad17b45e
mathieu-lin <mathieu.lin@outlook.fr>
2026-04-02 14:46:23 +0200

    test

--- src/test/java/com/example/mesSalonsDeCoiffure_api/repositories/ReservationRepositoryTest.java
+++ src/test/java/com/example/mesSalonsDeCoiffure_api/repositories/ReservationRepositoryTest.java
@@ -5,6 +5,7 @@ import com.example.mesSalonsDeCoiffure_api.entities.Utilisateur;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest 
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ReservationRepositoryTest {
 
     @Autowired
